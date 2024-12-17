28cabd212316fb440660fef57f9184e6cd712d22
Cross-devspec <leaveplace01@gmail.com>
2024-12-17 15:51:57 +0400

    Create main.py

---
+++ scripts/new.script/main.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+print("Example of script")
+sleep(1)
+print("Example of object")
+sleep(2)
+print("Example of new.script")
+sleep(3)
+print("Also add '.service' in service create 'version.txt'")
+
+abc = input(": ")
